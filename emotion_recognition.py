940def351595d144bf60376ebed21fe57ec3d188
Guru Rahul <gururahul123456789@gmail.com>
2024-11-04 11:18:24 +0530

    second commit

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -1,12 +1,12 @@
-from facial_emotion_recognition import EmotionRecognition
+from facial_emotion_recognition import EmotionRecognition # we are just importing the class
 import cv2
-er = EmotionRecognition(device='cpu')
+er = EmotionRecognition(device='cpu') # we are creating an variable of the class
 
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0) # This synrtax is used to acces the camera
 while True :
-    Success,frame =cam.read()
-    frame =er.recognise_emotion(frame,return_type='BGR')
-    cv2.imshow("Frame",frame)
+    Success,frame =cam.read() # This synrtax is used to read the camera
+    frame =er.recognise_emotion(frame,return_type='BGR') # This synrtax is used to detect the emotion 
+    cv2.imshow("Frame",frame)# This synrtax is used to display the frame
     key =cv2.waitKey(1)
     if key ==27:
         break
--- emotion_recognition_using_mobile.py
+++ emotion_recognition_using_mobile.py
@@ -3,11 +3,11 @@ from facial_emotion_recognition import EmotionRecognition
 import cv2
 import numpy as np
 
-er = EmotionRecognition(device='cpu')
+er = EmotionRecognition(device='cpu') # the value in the device == cpu is dependents upon the what we are entering in the serialization .py in the torch library
 
-url = "http://192.168.174.92:8080/shot.jpg"
+url = "Your IP for the camera" # this is the public IP of the camera
 while True :
-    imagePath = urllib.request.urlopen(url)
+    imagePath = urllib.request.urlopen(url) # It is used to open the url
     imageNp = np.array(bytearray(imagePath.read()),dtype=np.uint8)
     frame = cv2.imdecode(imageNp,-1)
     frame =er.recognise_emotion(frame,return_type='BGR')
